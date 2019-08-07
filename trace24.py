d59fec9c093b59566a7bee190525cc9c48d1ac41
apalom <35828580+apalom@users.noreply.github.com>
2019-08-07 17:51:05 -0600

    latest trace24

--- trace24.py
+++ trace24.py
@@ -25,12 +25,12 @@ data = pd.read_csv('hr_day_cnctd.csv',  index_col='Index');
 indiv_traces = {};
 
 # Convert categorical variables to integer
-hours = list(data.Hour)
+hours = np.linspace(0,23,24)
 n_hours = len(hours)
-x_lim = 16
+bins16 = np.linspace(0,15,16)
 
-out_yPred = pd.DataFrame(np.zeros((1,n_hours)), columns=list(hours))
-out_yObs = pd.DataFrame(np.zeros((1,n_hours)), columns=list(hours))
+out_yPred = np.zeros((n_hours,len(bins16)))
+out_yObs = np.zeros((n_hours,len(bins16)))
 
 for h in hours:
     print('Hour: ', h)
@@ -39,22 +39,25 @@ for h in hours:
         mu = pm.Uniform('mu', lower=0, upper=10)
 
         y_obs = data[data.Hour==h]['Connected'].values
+
         y_est = pm.NegativeBinomial('y_est', mu=mu, alpha=alpha, observed=y_obs)
 
         y_pred = pm.NegativeBinomial('y_pred', mu=mu, alpha=alpha)
 
-        trace = pm.sample(100, progressbar=True)
+        trace = pm.sample(500000, tune=2000, progressbar=True)
 
         #indiv_traces[h] = trace
 
     print('--- Observed ---')
-    print(np.histogram(data[data.Hour==h]['Connected'].values, bins=x_lim))
+    print(np.histogram(data[data.Hour==h]['Connected'].values, bins=bins16)[0])
     print('--- Predictive ---')
-    print(np.histogram(trace.get_values('y_pred'), bins=x_lim))
+    print(np.histogram(trace.get_values('y_pred'), bins=bins16)[0])
 
-    [out_yPred.loc[:,h], _] = np.histogram(trace.get_values('y_pred'), bins=x_lim)
-    [out_yObs.loc[:,h], _] = np.histogram(data[data.Hour==h]['Connected'].values, bins=x_lim)
+    out_yPred[h] = np.histogram(trace.get_values('y_pred'), bins=bins16)[0]
+    out_yObs[h] = np.histogram(data[data.Hour==h]['Connected'].values, bins=bins16)[0]
 
 # Export results
+out_yPred = pd.DataFrame(out_yPred)
+out_yObs = pd.DataFrame(out_yObs)
 out_yPred.to_csv('out_yPred.csv')
 out_yObs.to_csv('out_yObs.csv')
