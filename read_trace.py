1826c7e124da907579c11c2b9aa9fa6f11b9711d
apalom <35828580+apalom@users.noreply.github.com>
2019-08-08 16:32:01 -0600

    plot negbino

--- read_trace.py
+++ read_trace.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 import seaborns as sns
+import scipy.stats as stats
+from scipy.stats import nbinom, gamma, poisson
 
 # Read in trace data, hourly data seperated by sheet
 hours = np.linspace(0,23,24)
@@ -24,8 +26,22 @@ for h in hours:
     #trace24_smry_dict[h] = pd.read_excel('results/1188783_trace_xlsx_500smpl_25tune/out_trace.xlsx',  index_col=[0], sheetname=sheet)
     
 #%% Plot Hourly Value Distributions
+# https://docs.pymc.io/notebooks/GLM-negative-binomial-regression.html
 
+def get_nb_vals(mu, alpha, size):
+    """Generate negative binomially distributed samples by drawing a sample from a gamma 
+    distribution with mean `mu` and shape parameter `alpha', then drawing from a Poisson
+    distribution whose rate parameter is given by the sampled gamma variable."""    
 
+    g = stats.gamma.rvs(alpha, scale=mu / alpha, size=size)
+    return stats.poisson.rvs(g)
+
+mu = trace_smry['mean']['mu']
+alpha = trace_smry['mean']['alpha']
+
+plt.hist(get_nb_vals(mu, alpha, 1000), bins=np.arange(0,16), density=True, edgecolor='white', linewidth=1.2, label='Connected')
+#plt.set(xticks=np.arange(0,26,2), xlim=[-1, 25])
+plt.title('NegBino Trace')
 
 #%%
 
