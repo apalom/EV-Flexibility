8bcc88ca84b82e9916489324b24341ef870b6aa3
apalom <35828580+apalom@users.noreply.github.com>
2019-08-23 11:22:53 -0600

    create bayesFactor demo

---
+++ bayesFactor.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 10:42:49 2019
+
+@author: Alex
+"""
+
+import pymc3 as pm
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.special import betaln
+from scipy.stats import beta
+
+plt.style.use('seaborn-darkgrid')
+print('Running on PyMC3 v{}'.format(pm.__version__))
+
+def beta_binom(prior, y):
+    """
+    Compute the marginal likelihood, analytically, for a beta-binomial model.
+
+    prior : tuple
+        tuple of alpha and beta parameter for the prior (beta distribution)
+    y : array
+        array with "1" and "0" corresponding to the success and fails respectively
+    """
+    alpha, beta = prior
+    h = np.sum(y)
+    n = len(y)
+    p_y = np.exp(betaln(alpha + h, beta+n-h) - betaln(alpha, beta))
+    return p_y
+
+y = np.repeat([1, 0], [50, 50])  # 50 "heads" and 50 "tails"
+priors = ((1, 1), (30, 30))
+
+for a, b in priors:
+    distri = beta(a, b)
+    x = np.linspace(0, 1, 100)
+    x_pdf = distri.pdf(x)
+    plt.plot (x, x_pdf, label=r'$\alpha$ = {:d}, $\beta$ = {:d}'.format(a, b))
+    plt.yticks([])
+    plt.xlabel('$\\theta$')
+    plt.legend()
+    
+BF = (beta_binom(priors[1], y) / beta_binom(priors[0], y))
+print(round(BF))
